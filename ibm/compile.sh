5851ef58e3daf43c323750aafac95cfbfee500d3
Maxime Lalire <mlalire@cls.fr>
2019-05-21 10:27:49 +0200

    changement nom compile

--- ibm/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Run to compile Fortran-coded programs
-rm -f *.so *.mod *.pyc || exit 1
-/usr/bin/f2py -c --fcompiler=gnu95 -m mod_fx_fy mod_fx_fy.f90 || exit 1
-/usr/bin/f2py -c --fcompiler=gnu95 -m Advection2D_f Advection2D.f90 || exit 1
-
-
--- ibm/compile_gnu95.sh
+++ ibm/compile_gnu95.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# MACOSX 
-
 #Run to compile Fortran-coded programs
 rm -f *.so *.mod *.pyc || exit 1
-f2py -c --fcompiler=gfortran -m mod_fx_fy mod_fx_fy.f90 || exit 1
-f2py -c --fcompiler=gfortran -m Advection2D_f Advection2D.f90 || exit 1
+/usr/bin/f2py -c --fcompiler=gnu95 -m mod_fx_fy mod_fx_fy.f90 || exit 1
+/usr/bin/f2py -c --fcompiler=gnu95 -m Advection2D_f Advection2D.f90 || exit 1
 
 
